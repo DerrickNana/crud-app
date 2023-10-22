5955dff942d043f4f87c709bd58d9f7519b70b6c
NANA NDOKEP DERRICK <derrick.ndokep@yahoo.fr>
2023-10-21 23:05:23 -0400

    Update user entitie

--- src/main/java/ca/derrick/crudapplication/entities/User.java
+++ src/main/java/ca/derrick/crudapplication/entities/User.java
@@ -8,7 +8,8 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "user")
+@Entity
+@Table(name = "userTable")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
